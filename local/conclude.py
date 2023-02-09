3b0633753867a6a9869b3b76ee7684d9faca94b2
耿浩彭 <kevin@laronix.com>
2023-02-09 14:26:27 +0900

    VCP-67 add result conclusion

---
+++ local/conclude.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pathlib import Path
+import pdb
+
+result_dir = "./results"
+
+csvs = sorted(Path(result_dir).glob("**/*.csv"))
+
+datalist = []
+for x in csvs:
+    tmp = pd.read_csv(x, header=0)
+    datalist.append(tmp)
+
+dataset = pd.concat(datalist, ignore_index=False)
+
+means = dataset.groupby(" Method").mean()[' Nat'].values
+std = np.squeeze(dataset.groupby(" Method").std().values)
+id = [x.split("-")[0]  for x in dataset.groupby(" Method").mean().index]
+# pdb.set_trace()
+
+plt.bar(x=id, height=means, yerr=std, align="center", ecolor="r", capsize=0.5, alpha=0.5)
+plt.xlabel("Voices")
+plt.ylabel("MOS")
+plt.title("MOS Score with STD")
+# plt.errorbar(means, 0, xerr=sem, linestyle="None")
+plt.savefig("./results/nat.png")
+# pdb.set_trace()
